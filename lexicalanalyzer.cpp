441ec100e21e00a9c44d6e75acbb00afe5139ca5
xjphongx <JimmyPhong16@csu.fullerton.edu>
2019-09-19 14:02:24 -0700

    got the operator  tokens to work

--- lexicalanalyzer.cpp
+++ lexicalanalyzer.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <ctype.h>
+#include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -36,27 +37,7 @@ int main()
 
         lexer(line);
 
-        /*
-        stringstream test1(line);
-        string intermediate;
-
-        while(getline(test1, intermediate, ' '))
-        {
-            lineVector.push_back(intermediate);
-        }
         
-
-        //print out vector results
-        cout <<"...Testing whitespace seperation..."<<endl;
-        for (int i = 0; i < lineVector.size();i++)
-        {
-            cout << lineVector[i]<<endl;
-        }
-        cout << endl;
-        lineVector.clear();//refreshes the vector for the next line
-
-
-        */
     }
 
    
@@ -97,7 +78,7 @@ void lexer(string line)
     //this will do all the testing and tokening
     
     //check for operators first 
-    char operators[] = "+-*/%=";
+    char operators[] = "+-*/%=<>";
 
     //convert string to cstring to check each character 
     int length = line.length();
@@ -110,6 +91,41 @@ void lexer(string line)
         cout << chArray[a] << " ";
     }
 
+    //tokenizing the string below
+    cout << "...Testing tokenization of the string..."<<endl;
+    char buffer[15];//this will be inputed into keyWords function
+
+
+    /*
+        This could be an implementation of Finite State Machine
+        Initial State is at the source code input
+        Reads each character and catagorize them
+        Once done goes to the next state and repeats the process until no more inputs
+        Final State will be reached
+
+    */    
+    for (int b=0; b < length;b++)
+    {
+
+        if(chArray[b]== '+'||chArray[b]== '-'||chArray[b]== '*'||chArray[b]== '/'||
+        chArray[b]== '%'||chArray[b]== '='||chArray[b]== '<'||chArray[b]== '>')//check operator
+        {
+            cout << "operator"<<setw(15)<<chArray[b]<<endl;
+
+        }
+
+        if(chArray[b]== '['||chArray[b]== ']'||chArray[b]== '('||chArray[b]== ')'||
+        chArray[b]== '{'||chArray[b]== '}'||chArray[b]== '[')
+        {
+
+
+        }
+
+
+
+
+
+    }
 
 
 }
