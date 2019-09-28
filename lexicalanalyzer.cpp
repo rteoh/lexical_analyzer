40395cba3e1d78534aa888d36b8a261f554a246b
xjphongx <31776718+xjphongx@users.noreply.github.com>
2019-09-27 21:19:47 -0700

    changed char array into vectors and completed the lexical analyzer

--- lexicalanalyzer.cpp
+++ lexicalanalyzer.cpp
@@ -1,7 +1,7 @@
 /*
 Jimmy Phong
 Ryan Teoh
-Assignment 1 Lexical Analyzer 
+Assignment 1 Lexical Analyzer
 */
 
 
@@ -21,233 +21,203 @@ Assignment 1 Lexical Analyzer
 (achieve this by listing out all keywords in c++ by using if-else statement)
 */
 
-
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <vector>
 
 
 using namespace std;
 
-bool isKeyword(char buffer[]);
-string convertToString(char ch[], int size);
-void lexer(string line); //this checks EACH lexeme and returns a token
+bool isKeyword(vector<char>buffer);
+void lexer(vector<char>chVector, vector<char>buffer); //this checks EACH lexeme 
+//void operator(vector<char>buffer);
+//void seperator(vector<char>buffer);
 
 int main()
 {
+	// Open Input file
+	ifstream inputFile;
+	inputFile.open("test1.txt");
 
-    // Open Input file
-    ifstream inputFile;
-    inputFile.open("test1.txt");
+	//file error check
+	if (!inputFile.is_open())
+	{
+		cout << "error in file opening" << endl;
+		exit(0);
+	}
 
-    
-    //file error check
-    if(!inputFile.is_open())
-    {
-        cout << "error in file opening" <<endl;
-        exit(0);
-    }
+	// Create Header of Output File
+	ofstream outFile;
+	outFile.open("output.txt");
+	outFile << setw(22) << "Output: " << endl;
+	outFile << "TOKENS " << setw(27) << "Lexemes" << endl << endl;
+	outFile.close();
 
+	//vector initialization
+	vector<char> chVector;	//hold the characters
+	vector<char> buffer;	//pushes characters from chVector 
 
-    // Create Header of Output File
-    ofstream outFile;
-    outFile.open("output.txt");
-    outFile <<setw(22)<<"Output: "<<endl;
-    outFile <<"TOKENS "<<setw(27)<<"Lexemes"<<endl << endl;
-    outFile.close();
+	while (!inputFile.eof())
+	{
+		string line;
+		getline(inputFile, line);
+		cout << "...Testing getLine..." << endl;
+		cout << line << endl;//here i am inputing a whole string into the function
+		
+		//converts string and puts each char into a vector
+		cout << "...Testing Correct Vector Input..." << endl;
+		copy(line.begin(), line.end(), back_inserter(chVector));
+		for (int a = 0; a < chVector.size(); a++)
+		{
+			cout << chVector[a]<< " ";
+		}
+		cout << endl;
+		//passing the vector into the lexer function for tokenization
+		lexer(chVector,buffer);	//call the lexer function
+		chVector.clear();		//clears for next line
+	}
 
-
-    while(!inputFile.eof())
-    {  
-        vector<string> lineVector;
-        string line;
-        getline(inputFile,line);
-        cout << "...Testing getLine..."<<endl;
-        cout << line << endl;//here i am inputing a whole string into the function
-        lexer(line);//call the lexer function
-    }
-    
-
-
-    inputFile.close();
-    cout << "...Testing Complete..." <<endl;
-    cout<<"...Terminating Program..."<<endl;
-    return 0;
+	inputFile.close();
+	cout << "...Testing Complete..." << endl;
+	cout << "...Terminating Program..." << endl;
+	return 0;
 }
 
 //////////////////////Function Definitions////////////////////////////////
-/*
-    isKeyword function will return true or false if the input buffer is a keyword
-*/
-bool isKeyword(char buffer[])
+bool isKeyword(vector<char> buffer)
 {
-    char keywords[32][10]= {"auto","break","case","char","const","continue","default",
+	//convert buffer to string
+	string str(buffer.begin(), buffer.end());
+	cout <<"Buffer in String Form: " <<str << endl;
+	
+	string keywords[33] = { "auto","bool","break","case","char","const","continue","default",
 							"do","double","else","enum","extern","float","for","goto",
 							"if","int","long","register","return","short","signed",
 							"sizeof","static","struct","switch","typedef","union",
-							"unsigned","void","volatile","while"};
+							"unsigned","void","volatile","while" };
+	
+	//check if the word in the buffer is a keyword
+	bool indicator = false; // this is the initial response 
+	for (int c = 0; c < 33; c++)
+	{
+		if (str == keywords[c])
+		{
+			indicator = true;//set true if found 
+			cout << str << " is a keyword" << endl;
+		}
+		
+	}
 
-    //creating a variable to initcate if buffer is a keyword
-    bool indicator = false; // this is the initial response 
-    for (int b = 0; b < 32; b++)
-    {
-        if(strcmp(keywords[b],buffer))
-        {
-            indicator = true; // if keyword and buffer are the same, then true
-            break; //exits the loop
-        }
-    }
 
-    return indicator;
+	return indicator;
 
 }
-//converting character array to string for printing
-string convertToString(char ch[], int size)
+
+//void operator(vector<char>buffer)
+//{
+//	
+//}
+//void seperator(vector<char> buffer)
+//{
+//
+//
+//}
+//
+
+void lexer(vector<char>chVector,vector<char>buffer)
 {
-    string str = "";
-    for(int d = 0; d < size; d++)
-    {
-        str = str + ch[d];
-    }
-    return str;
-}
-void lexer(string line)
-{
-    //this will do all the testing and tokening
-    
-    //check for operators first 
-    char operators[] = "+-*/%=<>";
+	//check for operators first 
+	char operators[] = "+-*/%=<>";
 
-    //convert string to cstring to check each character 
-    int length = line.length();
-    char chArray[length+1];
-    strcpy(chArray,line.c_str());//this copys the string into char array
-
-    cout <<"...Testing string to char conversion..."<<endl;
-    for(int a = 0; a < length;a++)
-    {
-        cout << chArray[a] << " ";
-    }
+	//tokenizing the string below AND print out to file.
+	ofstream outFile;
+	outFile.open("output.txt", ios::app);
 
 
-
-    //tokenizing the string below AND print out to file.
-    ofstream outFile;
-    outFile.open("output.txt", ios::app);
-
-    char buffer[15];//this will be inputed into keyWords function
-    int position = 0;//this is the position in the buffer char array
-    int bufferSize;
-    string word;
-
-    /*
-        This could be an implementation of Finite State Machine
-        Initial State is at the source code input
-        Reads each character and catagorize them
-        Once done goes to the next state and repeats the process until no more inputs
-        Final State will be reached at output
-
-    */  
+	/*
+		This could be an implementation of Finite State Machine
+		Initial State is at the source code input
+		Reads each character and catagorize them
+		Once done goes to the next state and repeats the process until no more inputs
+		Final State will be reached at output
+	*/
 
 
-    // Check to see if line starts with a valid character
-    if(chArray[0] == '!') {
-    //if( !isalnum(chArray[0]) || (chArray[0] != '{') || (chArray[0] != '}') || (chArray[0] != '#') || (chArray[0] != '/')  || (chArray[0] != '*')) {
+	// Check to see if line starts with a valid character
+	if (chVector[0] == '!') {
+		//if( !isalnum(chArray[0]) || (chArray[0] != '{') || (chArray[0] != '}') || (chArray[0] != '#') || (chArray[0] != '/')  || (chArray[0] != '*')) {
 
-        // If line does not with alpha-numeric or with certain characters, the line will be ignored.
-        return;
+			// If line does not with alpha-numeric or with certain characters, the line will be ignored.
+		return;
 
-    } 
-    
+	}
 
-    for (int b=0; b < length;b++)
-    {
-        //checks operator
-        if(chArray[b]== '+'||chArray[b]== '-'||chArray[b]== '*'||chArray[b]== '/'||
-        chArray[b]== '%'||chArray[b]== '='||chArray[b]== '<'||chArray[b]== '>')
-        {
+	string wordInBuffer;
+	//This goes through chVector and adds into buffer if needed
+	for (int b = 0; b < chVector.size(); b++)
+	{
+		//checks operator
+		if (chVector[b] == '+' || chVector[b] == '-' || chVector[b] == '*' || chVector[b] == '/' ||
+			chVector[b] == '%' || chVector[b] == '=' || chVector[b] == '<' || chVector[b] == '>')
+		{
+			//TOOK OUT wordInBuffer here, not relevant
+			outFile << "OPERATOR         =" << setw(10) << chVector[b] << endl;
 
-            // If previous keyword/identifiers did not get called before operator
-            if(position != 0) {
+		}
+		else if (chVector[b] == '[' || chVector[b] == ']' || chVector[b] == '(' || chVector[b] == ')' ||
+			chVector[b] == '{' || chVector[b] == '}' || chVector[b] == '[' || chVector[b] == ',' || chVector[b] == ';')
+		{
+			// If previous keyword/identifiers did not get called before operator
+			if (!buffer.empty())
+			{
+				string wordInBuffer(buffer.begin(), buffer.end());
+				cout << "Current Word: " << wordInBuffer << endl;
+				if (isKeyword(buffer) == true)
+					outFile << "KEYWORD          =         " << wordInBuffer << endl;
+				else
+					outFile << "IDENTIFIER       =         " << wordInBuffer << endl;
 
-                // Convert char to string
-                bufferSize = sizeof(buffer);
-                word = convertToString(buffer,bufferSize);
+				// Resets Buffer
+				buffer.clear();
+			}
+			outFile << "SEPERATOR        =" << setw(10) << chVector[b] << endl;
+		}
+		else if (isalnum(chVector[b])|| chVector[b] == '$')
+		{
+			buffer.push_back(chVector[b]);
+			cout << "Inside Buffer: ";
+			for (int p = 0; p < buffer.size(); p++)
+			{
+				cout << buffer[p];
+			}
+			cout << endl;
+			
+		}
+		else if ((chVector[b] == ' ' || chVector[b] == '\n') && (!buffer.empty())) {
 
-                outFile << "KEYWORD          =         " << word <<endl;  
+			//// Convert char to string
+			//bufferSize = sizeof(buffer);
+			//word = convertToString(buffer, bufferSize);
 
-                // Resets Buffer
-                while(position != 0) {
-                    buffer[position]= '\0';
-                    position--;
-                }
+			string wordInBuffer(buffer.begin(), buffer.end());
+			cout << "Current Word: " << wordInBuffer << endl;
 
-            }
+			//once if condition is met, check the things inside buffer and compare
+			if (isKeyword(buffer) == true) 
+				outFile << "KEYWORD          =         " << wordInBuffer << endl;
+			else 
+				outFile << "IDENTIFIER       =         " << wordInBuffer << endl;
 
-            outFile << "OPERATOR         =" << setw(10) << chArray[b]<<endl;
+			// Resets Buffer
+			buffer.clear();
 
+		}
 
-        }
-        else if(chArray[b]== '['||chArray[b]== ']'||chArray[b]== '('||chArray[b]== ')'||
-        chArray[b]== '{'||chArray[b]== '}'||chArray[b]== '['||chArray[b]== ','||chArray[b]== ';')
-        {
-
-            // If previous keyword/identifiers did not get called before operator
-            if(position != 0) {
-
-                // Convert char to string
-                bufferSize = sizeof(buffer);
-                word = convertToString(buffer,bufferSize);
-
-                outFile << "KEYWORD          =         " << word <<endl;  
-
-                // Resets Buffer
-                while(position != 0) {
-                    buffer[position]= '\0';
-                    position--;
-                }
-
-            }
-
-            outFile <<"SEPERATOR        =" << setw(10) << chArray[b]<<endl;
-
-
-        // If char is not space or new line, add char to buffer
-        } else if(isalnum(chArray[b])) {
-
-            buffer[position++]=chArray[b];
-
-        } else if((chArray[b] == ' '||chArray[b]=='\n') && (position !=0)) { 
-
-            // Convert char to string
-            bufferSize = sizeof(buffer);
-            word = convertToString(buffer,bufferSize);
-
-
-
-            //once if condition is met, check the things inside buffer and compare
-            if(isKeyword(buffer) == true) {
-
-                outFile << "KEYWORD          =         " << word <<endl;  
-
-            } else {
-
-                outFile << "IDENTIFIERS      =         " << word <<endl;
-
-            }
-
-            // Resets Buffer
-            while(position != 0) {
-                buffer[position]= '\0';
-                position--;
-            }
-
-        }
-
-    }
-    outFile.close();
+	}
+	outFile.close();
 
 }
